b50183ba3c4fce1ef142354066c54a24d8e85215
Mshivam2409 <shivammalhotra08@gmail.com>
2020-07-08 17:37:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+// styles for this kit
+import "assets/css/bootstrap.min.css";
+import "assets/scss/now-ui-kit.scss?v=1.4.0";
+import "assets/demo/demo.css?v=1.4.0";
+import "assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+import Home from "./pages/Home";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  MemoryRouter,
+  HashRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        {/* <Route path="/research">
+          <ProfilePage />
+        </Route> */}
+      </Switch>
+      <Redirect to="/home"></Redirect>
+    </HashRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+// reactstrap components
+// import {
+// } from "reactstrap";
+
+// core components
+// import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import HomeHeader from "../components/HomeHeader";
+import HomeNavbar from "components/HomeNavBar";
+// import DarkFooter from "components/Footers/DarkFooter.js";
+
+function Home() {
+  React.useEffect(() => {
+    document.body.classList.add("index-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("index-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
+  return (
+    <>
+      <HomeNavbar />
+      <div className="wrapper">
+        <HomeHeader />
+        <div className="main">
+          {/* <Images /> */}
+          {/* <BasicElements /> */}
+          {/* <Navbars /> */}
+          {/* <Tabs /> */}
+          {/* <Pagination /> */}
+          {/* <Notifications /> */}
+          {/* <Typography /> */}
+          {/* <Javascript /> */}
+          {/* <Carousel /> */}
+          {/* <NucleoIcons /> */}
+          {/* <CompleteExamples /> */}
+          {/* <SignUp /> */}
+          {/* <Examples /> */}
+          {/* <Download /> */}
+        </div>
+        {/* <DarkFooter /> */}
+      </div>
+    </>
+  );
+}
+
+export default Home;

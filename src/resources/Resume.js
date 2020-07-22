846e2914277e386aa07a20e6333d05f96476eb9e
Mshivam2409 <shivammalhotra08@gmail.com>
2020-07-22 19:30:44 +0530

    Add Resume

--- src/resources/Resume.js
+++ src/resources/Resume.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // reactstrap components
 import {
@@ -16,12 +16,13 @@ import {
 import firebase from "firebase";
 
 //ResourceData
-const Resumes = [
+var Resumes = [
   {
     name: "Analytics",
     resumes: [
       "gs://careerportal-816f6.appspot.com/170738_2 - Sunit Gautam.pdf",
     ],
+    urls: [],
   },
   {
     name: "Core",
@@ -30,6 +31,7 @@ const Resumes = [
       "gs://careerportal-816f6.appspot.com/MITSUBISHI2019233(1) - Aditya Tiwari.pdf",
       "gs://careerportal-816f6.appspot.com/cv-onepage - Aniket Sanghi.pdf",
     ],
+    urls: [],
   },
   {
     name: "Finance",
@@ -38,10 +40,12 @@ const Resumes = [
       "gs://careerportal-816f6.appspot.com/170520_2.pdf - Raghav Maheshwari.pdf",
       "gs://careerportal-816f6.appspot.com/170817_1 (1) - Yash Maheshwari.pdf",
     ],
+    urls: [],
   },
   {
     name: "Fintech",
     resumes: ["gs://careerportal-816f6.appspot.com/170137_3 - Anuj Shah.pdf"],
+    urls: [],
   },
   {
     name: "HFT & Quant",
@@ -50,6 +54,7 @@ const Resumes = [
       "gs://careerportal-816f6.appspot.com/Akshat Agrawal - akshat agrawal.pdf",
       "gs://careerportal-816f6.appspot.com/CV - Jaydeep Goyal.pdf",
     ],
+    urls: [],
   },
   //   {
   //     name: "Research Intern",
@@ -65,6 +70,7 @@ const Resumes = [
       "gs://careerportal-816f6.appspot.com/Resume__Yash_Baheti - Yash Baheti.pdf",
       "gs://careerportal-816f6.appspot.com/umang-malik - Umang Malik.pdf",
     ],
+    urls: [],
   },
   {
     name: "Tehnomanagerial",
@@ -72,6 +78,7 @@ const Resumes = [
       "gs://careerportal-816f6.appspot.com/umang-malik - Umang Malik.pdf",
       "gs://careerportal-816f6.appspot.com/Pence_Resume_final - Pence Mataria.pdf",
     ],
+    urls: [],
   },
 ];
 
@@ -79,72 +86,90 @@ const Resumes = [
 const Resume = () => {
   var storage = firebase.storage();
   const [iconPills, setIconPills] = React.useState("0");
+  const [isLoading, setisLoading] = useState(false);
+  useEffect(() => {
+    const getUrls = async () => {
+      setisLoading(true);
+      for (let index = 0; index < Resumes.length; index++) {
+        var category = Resumes[index];
+        for (let i = 0; i < category.resumes.length; i++) {
+          var resume = category.resumes[i];
+          var gsReference = storage.refFromURL(resume);
+          const url = await gsReference.getDownloadURL();
+          category.urls.push(url);
+        }
+      }
+      setisLoading(false);
+    };
+    getUrls();
+  }, []);
   return (
     <>
-      <Container>
-        <Row>
-          <p className="category">Sample Resumes</p>
-          <Card>
-            <CardHeader>
-              <Nav className="justify-content-center" role="tablist" tabs>
-                {Resumes.map((resource, index) => {
-                  return (
-                    <NavItem>
-                      <NavLink
-                        className={
-                          iconPills === index.toString() ? "active" : ""
-                        }
-                        href="#pablo"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          setIconPills(index.toString());
-                        }}
-                      >
-                        {resource.name}
-                      </NavLink>
-                    </NavItem>
-                  );
-                })}
-              </Nav>
-            </CardHeader>
-            <CardBody>
-              <TabContent
-                className="text-center"
-                activeTab={"iconPills" + iconPills}
-              >
-                {Resumes.map((resource, index) => {
-                  return (
-                    <TabPane tabId={"iconPills" + index.toString()}>
-                      <p class="h5" color="dark">
-                        <ul>
-                          {resource.resumes.map(async (resume, index) => {
-                            var gsReference = storage.refFromURL(resume);
-                            const url = await gsReference.getDownloadURL();
-                            return (
-                              <li style={{ textAlign: "left" }}>
-                                <a
-                                  href={url}
-                                  style={{
-                                    textDecoration: "none",
-                                    color: "black",
-                                  }}
-                                  download
-                                >
-                                  {"Sample " + (index + 1)}
-                                </a>
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      </p>
-                    </TabPane>
-                  );
-                })}
-              </TabContent>
-            </CardBody>
-          </Card>
-        </Row>
-      </Container>
+      {isLoading && <div></div>}
+      {!isLoading && (
+        <Container>
+          <Row>
+            <p className="category">Sample Resumes</p>
+            <Card>
+              <CardHeader>
+                <Nav className="justify-content-center" role="tablist" tabs>
+                  {Resumes.map((resource, index) => {
+                    return (
+                      <NavItem>
+                        <NavLink
+                          className={
+                            iconPills === index.toString() ? "active" : ""
+                          }
+                          href="#pablo"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setIconPills(index.toString());
+                          }}
+                        >
+                          {resource.name}
+                        </NavLink>
+                      </NavItem>
+                    );
+                  })}
+                </Nav>
+              </CardHeader>
+              <CardBody>
+                <TabContent
+                  className="text-center"
+                  activeTab={"iconPills" + iconPills}
+                >
+                  {Resumes.map((resource, index) => {
+                    return (
+                      <TabPane tabId={"iconPills" + index.toString()}>
+                        <p class="h5" color="dark">
+                          <ul>
+                            {resource.urls.map(async (url, index) => {
+                              return (
+                                <li style={{ textAlign: "left" }}>
+                                  <a
+                                    href={url}
+                                    style={{
+                                      textDecoration: "none",
+                                      color: "black",
+                                    }}
+                                    download
+                                  >
+                                    {"Sample " + (index + 1)}
+                                  </a>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </p>
+                      </TabPane>
+                    );
+                  })}
+                </TabContent>
+              </CardBody>
+            </Card>
+          </Row>
+        </Container>
+      )}
     </>
   );
 };

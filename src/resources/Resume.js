3251660d0043bad03a73f76989510f419d8d5079
Mshivam2409 <shivammalhotra08@gmail.com>
2020-09-05 09:06:07 +0530

    Update Resumes

--- src/resources/Resume.js
+++ src/resources/Resume.js
@@ -26,7 +26,7 @@ var Resumes = [
       "gs://careerportal-816f6.appspot.com/Placement_one_page - prajual maheshwari.pdf",
       "gs://careerportal-816f6.appspot.com/160659_5 - Shivank Khard.pdf",
       "gs://careerportal-816f6.appspot.com/160010_1 - Abhay Pratap Singh.pdf",
-      "gs://careerportal-816f6.appspot.com/AyushreeGupta - Ayushree Gupta.pdf"
+      "gs://careerportal-816f6.appspot.com/AyushreeGupta - Ayushree Gupta.pdf",
     ],
     urls: [],
   },
@@ -40,7 +40,7 @@ var Resumes = [
       "gs://careerportal-816f6.appspot.com/Resume_Core - Anant Chopra.pdf",
       "gs://careerportal-816f6.appspot.com/JLR_Resume_Aditya - Aditya Rajawat.pdf",
       "gs://careerportal-816f6.appspot.com/15807182_1 - Bhanupratap Niranjan.pdf",
-      "gs://careerportal-816f6.appspot.com/inbound1495326740525160075 - Sumit Kumar.pdf"
+      "gs://careerportal-816f6.appspot.com/inbound1495326740525160075 - Sumit Kumar.pdf",
     ],
     urls: [],
   },
@@ -65,7 +65,7 @@ var Resumes = [
       "gs://careerportal-816f6.appspot.com/170191_2 (1) - Ayush Gupta.pdf",
       "gs://careerportal-816f6.appspot.com/Akshat Agrawal - akshat agrawal.pdf",
       "gs://careerportal-816f6.appspot.com/CV - Jaydeep Goyal.pdf",
-      "gs://careerportal-816f6.appspot.com/Hunar_Resume - Hunarpreet Singh.pdf"
+      "gs://careerportal-816f6.appspot.com/Hunar_Resume - Hunarpreet Singh.pdf",
     ],
     urls: [],
   },
@@ -90,7 +90,7 @@ var Resumes = [
       "gs://careerportal-816f6.appspot.com/160299_2 - adarsh honakamble.pdf",
       "gs://careerportal-816f6.appspot.com/inbound1364827606138206469 - jenil mewada.pdf",
       "gs://careerportal-816f6.appspot.com/Utkarsh Barsaiyan Consulting Resume - Utkarsh Barsaiyan.pdf",
-      "gs://careerportal-816f6.appspot.com/Resume_Dev - Sahil Dhull.pdf"
+      "gs://careerportal-816f6.appspot.com/Resume_Dev - Sahil Dhull.pdf",
     ],
     urls: [],
   },
@@ -106,55 +106,89 @@ var Resumes = [
   },
 ];
 
+const Drive = [
+  {
+    name: "Aerospace Core",
+    drive:
+      "https://drive.google.com/drive/folders/1WLd8aiBoi4e_X8sCB5zH4SFSTrDLUxb8?usp=sharing",
+  },
+  {
+    name: "Analytics",
+    drive:
+      "https://drive.google.com/drive/folders/1KFeH27Yvqj8x8f4EfIJ3J8OLsBbuXUd3?usp=sharing",
+  },
+  {
+    name: "Chemical Core",
+    drive:
+      "https://drive.google.com/drive/folders/1eTjMBCkps5fzzJacZk3J9c5aF2C0PJEJ?usp=sharing",
+  },
+  {
+    name: "Consulting",
+    drive:
+      "https://drive.google.com/drive/folders/1jftp24f_QZRPbRfxptmln99-i2qtm4bW?usp=sharing",
+  },
+  {
+    name: "CSE Core",
+    drive:
+      "https://drive.google.com/drive/folders/1BkBFkUjVI48DVmYeTwy_Y7t7EYSs3wwT?usp=sharing",
+  },
+  {
+    name: "Electrical Core",
+    drive:
+      "https://drive.google.com/drive/folders/1buacY7vfgAvfYcWHaj1Dg4xt_A-RB9w4?usp=sharing",
+  },
+  {
+    name: "HFT and Quant",
+    drive:
+      "https://drive.google.com/drive/folders/1da6-Eq49jqQ6_lKfdKTdkx7X24-g-wmg?usp=sharing",
+  },
+  {
+    name: "Mechanical Core",
+    drive:
+      "https://drive.google.com/drive/folders/18zGkRyoEzMXLOwDARSnT8Zyp7nZso8H0?usp=sharing",
+  },
+  {
+    name: "MSE Core",
+    drive:
+      "https://drive.google.com/drive/folders/18Y6J7MTZa36kItTI1oxMeIInSycMS7zE?usp=sharing",
+  },
+  {
+    name: "Software",
+    drive:
+      "https://drive.google.com/drive/folders/1ovnaJOi6dh35IeQGnfYzempzMrmK2fPf?usp=sharing",
+  },
+  {
+    name: "UX Designer",
+    drive:
+      "https://drive.google.com/drive/folders/1cNGXKZzNpVZC_re6wKo1yP6t3Dpaa2kr?usp=sharing",
+  },
+];
+
 // core components
 const Resume = () => {
-  var storage = firebase.storage();
   const [iconPills, setIconPills] = React.useState("0");
-  const [isLoading, setisLoading] = useState(false);
-  useEffect(() => {
-    const getUrls = async () => {
-      setisLoading(true);
-      for (let index = 0; index < Resumes.length; index++) {
-        var category = Resumes[index];
-        for (let i = 0; i < category.resumes.length; i++) {
-          var resume = category.resumes[i];
-          var gsReference = storage.refFromURL(resume);
-          const url = await gsReference.getDownloadURL();
-          category.urls.push(url);
-        }
-      }
-      setisLoading(false);
-    };
-    getUrls();
-  }, []);
+
   return (
     <>
-      {isLoading && <div></div>}
-      {!isLoading && (
+      {
         <Container>
           <Row>
             <p className="category">Sample Resumes</p>
             <Card>
               <CardHeader>
                 <Nav className="justify-content-center" role="tablist" tabs>
-                  {Resumes.map((resource, index) => {
-                    return (
-                      <NavItem>
-                        <NavLink
-                          className={
-                            iconPills === index.toString() ? "active" : ""
-                          }
-                          href="#pablo"
-                          onClick={(e) => {
-                            e.preventDefault();
-                            setIconPills(index.toString());
-                          }}
-                        >
-                          {resource.name}
-                        </NavLink>
-                      </NavItem>
-                    );
-                  })}
+                  <NavItem>
+                    <NavLink
+                      className={"active"}
+                      href="#pablo"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setIconPills(index.toString());
+                      }}
+                    >
+                      Resumes
+                    </NavLink>
+                  </NavItem>
                 </Nav>
               </CardHeader>
               <CardBody>
@@ -162,38 +196,33 @@ const Resume = () => {
                   className="text-center"
                   activeTab={"iconPills" + iconPills}
                 >
-                  {Resumes.map((resource, index) => {
-                    return (
-                      <TabPane tabId={"iconPills" + index.toString()}>
-                        <p class="h5" color="dark">
-                          <ul>
-                            {resource.urls.map((url, index) => {
-                              return (
-                                <li style={{ textAlign: "left" }}>
-                                  <a
-                                    href={url}
-                                    style={{
-                                      textDecoration: "none",
-                                      color: "black",
-                                    }}
-                                    download
-                                  >
-                                    {"Sample " + (index + 1)}
-                                  </a>
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </p>
-                      </TabPane>
-                    );
-                  })}
+                  <TabPane tabId={"iconPills" + "0"}>
+                    <p class="h5" color="dark">
+                      <ul>
+                        {Drive.map((resource, index) => {
+                          return (
+                            <li style={{ textAlign: "left" }}>
+                              <a
+                                href={resource.drive}
+                                style={{
+                                  textDecoration: "none",
+                                  color: "black",
+                                }}
+                              >
+                                {"View Samples "}
+                              </a>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </p>
+                  </TabPane>
                 </TabContent>
               </CardBody>
             </Card>
           </Row>
         </Container>
-      )}
+      }
     </>
   );
 };
